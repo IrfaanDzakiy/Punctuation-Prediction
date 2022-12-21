17278692db1ab4f3681430e661e82701eb2852e3
Irfaan Dzakiy <13518145@std.stei.itb.ac.id>
2022-12-21 11:51:00 +0700

    fix undersample script

--- src/data/undersample_text.py
+++ src/data/undersample_text.py
@@ -7,6 +7,20 @@ import warnings
 import os
 warnings.simplefilter(action='ignore', category=FutureWarning)
 
+Q_TARGET_COUNT = {
+    "bppt": 171,
+    "conllu_all_uncased": 2583127,
+    "frog_storytelling": 21,
+    "jw300": 74215,
+    "kompas": 2130,
+    "opensubtitles": 4040026,
+    "oscar_all_uncased": 4868458,
+    "parallel_corpus": 254,
+    "talpco_indonesia": 68,
+    "tempo": 2318,
+    "wiki": 9749,
+    "wikipedia_conllu": 19125
+}
 
 def remove_punc(word):
     selected_punc = string.punctuation.translate(str.maketrans('', '', ",.?"))
@@ -14,9 +28,15 @@ def remove_punc(word):
 
 
 def undersample_file(filepath):
-    df = pd.DataFrame()
+    input_filename = os.path.split(filepath)[-1].split('.')[0]
+    output_filepath = f"data/interim/indo4b/{input_filename}_undersampled.txt"
+
+    os.makedirs(os.path.dirname(output_filepath), exist_ok=True)
+    output_file = open(output_filepath, 'w', encoding='utf8')
 
     with open(filepath, 'r', encoding='utf8') as f:
+        dec_count = 0
+        ques_count = 0
         for line in f:
             strip_line = line.strip()
             if (strip_line == ""):
@@ -24,31 +44,14 @@ def undersample_file(filepath):
             if (strip_line[-1] not in [".", "?"]):
                 continue
             cleaned_line = remove_punc(strip_line)
-
-            row = {'Text': cleaned_line}
-            if (cleaned_line[-1] == '.'):
-                row['Label'] = 'declarative'
+            if (cleaned_line[-1] == '.' and dec_count < Q_TARGET_COUNT[input_filename]):
+                dec_count += 1
+                output_file.write(f"{cleaned_line}\n")
             elif (cleaned_line[-1] == '?'):
-                row['Label'] = 'question'
-            df = df.append(row, ignore_index=True)
+                ques_count += 1
+                output_file.write(f"{cleaned_line}\n")
 
-    print(df.head())
-    print(df['Label'].value_counts())
-
-    X = df.drop('Label', axis=1)
-    y = df['Label']
-
-    undersample = RandomUnderSampler(sampling_strategy='majority')
-    X_res, y_res = undersample.fit_resample(X, y)
-
-    print(y_res.value_counts())
-
-    input_filename = os.path.split(filepath)[-1].split('.')[0]
-    output_filepath = f"data/interim/indo4b/{input_filename}_undersampled.txt"
-
-    os.makedirs(os.path.dirname(output_filepath), exist_ok=True)
-    np.savetxt(output_filepath, X_res.values, fmt='%s')
-
+    output_file.close()
 
 @click.command()
 @click.argument('input_dir', type=click.Path(exists=True))
@@ -58,9 +61,9 @@ def main(input_dir):
         for filename in filenames:
             if filename.endswith('.txt'):
                 files.append(os.sep.join([dirpath, filename]))
-    for file in files:
-        print(file)
-        undersample_file(file)
+    # for file in files:
+    #     print(file)
+    undersample_file("data/raw/indo4b/bppt.txt")
 
 
 if (__name__ == "__main__"):
