2e207f2f0b11b9ca514171b7cd666b7066f08d28
Irfaan Dzakiy <13518145@std.stei.itb.ac.id>
2022-12-26 20:54:10 +0700

    olk

--- src/features/generate_nmt_dataset.py
+++ src/features/generate_nmt_dataset.py
@@ -5,67 +5,28 @@ import string
 INPUT_DIR_PATH = 'data/raw/indo4b'
 OUTPUT_DIR_PATH = 'data/processed/nmt'
 
+def remove_punc(word):
+    return word.translate(str.maketrans('', '', string.punctuation))
 
 @click.command()
-@click.argument("filename", type=click.Path())
-def main(filename):
-    out_foldername = filename.split('.')[0]
-    out_path = os.path.join(OUTPUT_DIR_PATH, out_foldername)
+@click.argument("input_file", type=click.Path(exists=True))
+def main(input_file):
+    _, filename = os.path.split(input_file)
+    src_filepath = os.sep.join([OUTPUT_DIR_PATH, f"src-{filename}"])
+    tgt_filepath = os.sep.join([OUTPUT_DIR_PATH, f"tgt-{filename}"])
 
-    # os.makedirs(out_path)
-    src_train_file = open(os.path.join(
-        out_path, "src-train.txt"), 'w', encoding='utf8')
-    tgt_train_file = open(os.path.join(
-        out_path, "tgt-train.txt"), 'w', encoding='utf8')
-    src_val_file = open(os.path.join(
-        out_path, "src-val.txt"), 'w', encoding='utf8')
-    tgt_val_file = open(os.path.join(
-        out_path, "tgt-val.txt"), 'w', encoding='utf8')
-    src_test_file = open(os.path.join(
-        out_path, "src_test.txt"), 'w', encoding='utf8')
+    os.makedirs(os.path.dirname(src_filepath), exist_ok=True)
+    os.makedirs(os.path.dirname(tgt_filepath), exist_ok=True)
+    src_file = open(src_filepath, 'w', encoding='utf8')
+    tgt_file = open(tgt_filepath, 'w', encoding='utf8')
 
-    with open(os.path.join(INPUT_DIR_PATH, filename), "r", encoding='utf8') as f:
-        train_count = 0
-        val_count = 0
-        test_count = 0
+    with open(input_file, 'r', encoding='utf8') as f:
+        for line in f:
+            src_file.write(f"{remove_punc(line).strip()}\n")
+            tgt_file.write(line)
 
-        while train_count < 10000:
-            line = f.readline()
-            if (line == '\n'):
-                continue
-            tgt_train_file.write(line)
-            line_word_only = line
-            for char in string.punctuation:
-                line_word_only = line_word_only.replace(char, '')
-            src_train_file.write(line_word_only)
-            train_count += 1
-
-        while val_count < 1000:
-            line = f.readline()
-            if (line == '\n'):
-                continue
-            tgt_val_file.write(line)
-            line_word_only = line
-            for char in string.punctuation:
-                line_word_only = line_word_only.replace(char, '')
-            src_val_file.write(line_word_only)
-            val_count += 1
-
-        while test_count < 1000:
-            line = f.readline()
-            if (line == '\n'):
-                continue
-            line_word_only = line
-            for char in string.punctuation:
-                line_word_only = line_word_only.replace(char, '')
-            src_test_file.write(line_word_only)
-            test_count += 1
-
-    src_train_file.close()
-    tgt_train_file.close()
-    src_val_file.close()
-    tgt_val_file.close()
-    src_test_file.close()
+    src_file.close()
+    tgt_file.close()
 
 
 if (__name__ == "__main__"):
